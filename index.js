be435810f820f71828b6ff2b42273ad82915dfb4
MalbonaGenio <pvalverdemolina@gmail.com>
2021-02-17 06:49:01 +0000

    added getclue fucntion to set the row start for each clue accordign to the clue value. Now the getCategories fetch also accepts count and ofset as arguments

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-/* 
-    Jeopardy
-    
-    Write an async function 
-        that uses fetch() to fetch 4 categories 
-        from https://jservice.io/api/categories
-        parameters: count, offset
-        
-    Display the categories
-        in a simplified 4x5 Jeopardy Board 
-        using CSS Grid
-*/
-
-async function getCategories() {
-    let response = await fetch("https://jservice.io/api/categories?count=4&offset=30")
+async function getCategories(count, offset) {
+    let response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
     let data = await response.json()
     return data
 }
+function getClueHtml(clueValue){
+  return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
+}
 
-function getCategoryHTML(category){
+function getCategoryHtml(category){
   return`
     <div class="my-category-title">${category.title}</div>
-    <div class="my-category-clue" style="grid-row-start: 2">$100</div>
-    <div class="my-category-clue" style="grid-row-start: 3">$200</div>
-    <div class="my-category-clue" style="grid-row-start: 4">$300</div>
-    <div class="my-category-clue" style="grid-row-start: 5">$400</div>`
+    ${getClueHtml(100)}
+    ${getClueHtml(200)}
+    ${getClueHtml(300)}
+    ${getClueHtml(400)}`
 }
-getCategories().then(categories => {
+getCategories(5).then(categories => {
   console.log(categories)
   document.body.innerHTML = `<div class="board">
-    ${categories.map(getCategoryHTML).join('')}
+    ${categories.map(getCategoryHtml).join('')}
   </div>`
 })
