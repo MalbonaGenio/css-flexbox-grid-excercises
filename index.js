a420418f931156553569f6ee8ff9ad49e895142a
MalbonaGenio <pvalverdemolina@gmail.com>
2021-02-17 05:14:38 +0000

    added the grid styles to arragne the categories and the clues

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ async function getCategories() {
 }
 
 function getCategoryHTML(category){
-  return`<div class="board">${category.title}</div>`
+  return`
+    <div class="my-category-title">${category.title}</div>
+    <div class="my-category-clue" style="grid-row-start: 2">$100</div>
+    <div class="my-category-clue" style="grid-row-start: 3">$200</div>
+    <div class="my-category-clue" style="grid-row-start: 4">$300</div>
+    <div class="my-category-clue" style="grid-row-start: 5">$400</div>`
 }
 getCategories().then(categories => {
   console.log(categories)
